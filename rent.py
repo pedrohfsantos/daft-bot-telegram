16808224b323ef408b2282ec41b1d4017619626b
Pedro Henrique <pedro.hfsantos7@gmail.com>
2022-05-06 06:54:21 -0300

    Add rent.ie

--- rent.py
+++ rent.py
@@ -3,36 +3,40 @@ import time
 from requests_html import HTMLSession
 
 session = HTMLSession()
-
 key_api = ""
+
 
 def send_message(chat_id, message):
     try:
         data = {"chat_id": chat_id, "text": message}
-        url = f"https://api.telegram.org/bot{key_api}/sendMessage"
+        url = f"https://api.telegram.org/{key_api}/sendMessage"
         session.post(url, data)
     except Exception as e:
         print("Erro no sendMessage:", e)
 
-def find_daft():
-    url = "https://www.daft.ie/sharing/ireland?rentalPrice_from=200&rentalPrice_to=650&firstPublishDate_from=now-1d%2Fd&suitableFor=male&location=dublin-city&location=dublin"
+def find_rent():
+    url = "https://www.rent.ie/rooms-to-rent/renting_dublin/room-type_either/rent_200-550/"
     r = session.get(url)
-    search_result = r.html.xpath('//ul[@data-testid="results"]/li/a/@href')
+    search_result = r.html.xpath('//div[@class="search_result"]//a/@href')
 
-    with open("sented_daft.txt", "r") as date:
+    with open("sented_rent.txt", "r") as date:
         all_send = date.readlines()
         all_send = [x.strip("\n") for x in all_send]
+
+    search_result = search_result[0:10]
 
     for result in search_result:
         if result in all_send:
             search_result.remove(result)
         else:
-            send_message("-619768044", f"https://www.daft.ie{result}")
-            with open("sented_daft.txt", "a") as date:
+            send_message("-619768044", f"{result}")
+            with open("sented_rent.txt", "a") as date:
                 date.write(f"{result}\n")
 
 
-schedule.every(2).minutes.do(find_daft)
+
+
+schedule.every(2).minutes.do(find_rent)
 
 
 while 1:
@@ -44,4 +48,4 @@ while 1:
     time.sleep(1)
 
 
-# https://api.telegram.org/bot{key_api}/getUpdates
+# https://api.telegram.org/{key_api}/getUpdates
